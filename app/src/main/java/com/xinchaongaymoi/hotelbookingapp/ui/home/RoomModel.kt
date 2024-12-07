f53defbd8bbd50f2fc193361d591fd3a067fc0d4
Le Thanh Vinh <thanhvinh2400@gmail.com>
2024-12-07 21:18:53 +0700

    implement home ui and room item

---
+++ app/src/main/java/com/xinchaongaymoi/hotelbookingapp/ui/home/RoomModel.kt
@@ -0,0 +1,11 @@
+package com.xinchaongaymoi.hotelbookingapp.ui.home
+
+
+data class Room(
+    val id: String = "",
+    val name: String = "",
+    val price: Int = 0,
+    val  sale :Float,
+    val rating :Float,
+    val imageUrl: String = ""
+)
