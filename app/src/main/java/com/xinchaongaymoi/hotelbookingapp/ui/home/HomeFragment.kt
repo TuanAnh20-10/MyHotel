528b08d621b7e98f3128081e8a56c7d6ff3b3eb1
quanphantien <22120369@student.hcmus.edu.vn>
2024-12-17 12:25:40 +0700

    Đổi trang Home sang trang Search.

--- app/src/main/java/com/xinchaongaymoi/hotelbookingapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/xinchaongaymoi/hotelbookingapp/ui/home/HomeFragment.kt
@@ -1,22 +1,13 @@
 package com.xinchaongaymoi.hotelbookingapp.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
-import com.xinchaongaymoi.hotelbookingapp.SearchActivity
-import com.xinchaongaymoi.hotelbookingapp.data.adapter.HomeRoomAdapter
-import com.xinchaongaymoi.hotelbookingapp.data.service.RoomService
-
 import com.xinchaongaymoi.hotelbookingapp.databinding.FragmentHomeBinding
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
 
 class HomeFragment : Fragment() {
 
@@ -24,60 +15,27 @@ private var _binding: FragmentHomeBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
-    private lateinit var  luxuryRoomRecyclerView:RecyclerView
-    private lateinit var  royalRoomRecyclerView:RecyclerView
-    private lateinit var luxuryAdapter: HomeRoomAdapter
-    private lateinit var royalAdapter:HomeRoomAdapter
-    private val roomService =RoomService()
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    val accountViewModel =
+            ViewModelProvider(this).get(AccountViewModel::class.java)
 
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
-      val searchBtn :Button = binding.searchBtn
-      val searchET :EditText = binding.searchET
-      searchBtn.setOnClickListener{
-          val intent =Intent(requireContext(),SearchActivity::class.java)
-          intent.putExtra("keyWord",searchET.text.toString())
-          startActivity(intent)
-      }
-
-      royalRoomRecyclerView= binding.recommendRV
-      luxuryRoomRecyclerView =binding.bestRV
-      royalRoomRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-      luxuryRoomRecyclerView.layoutManager=LinearLayoutManager(requireContext())
-      getRooms()
     val root: View = binding.root
 
-    homeViewModel.text.observe(viewLifecycleOwner) {
-
+    val textView: TextView = binding.textSlideshow
+    accountViewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
     }
     return root
-
   }
 
 override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-    private fun getRooms(){
-        Log.i("Hello","Hello")
-        roomService.getRoomByType("Luxury",
-            callback = {
-                roomList->luxuryAdapter=HomeRoomAdapter(roomList)
-                luxuryRoomRecyclerView.adapter=luxuryAdapter
-            }
-            )
-        roomService.getRoomByType("Royal",
-            callback = {
-                    roomList->royalAdapter=HomeRoomAdapter(roomList)
-                royalRoomRecyclerView.adapter=royalAdapter
-            }
-        )
     }
 }
