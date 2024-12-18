14036504cddbc20fc935b8ba41ae95e87c474263
quanphantien <22120369@student.hcmus.edu.vn>
2024-12-18 11:03:30 +0700

    Cập nhật thêm trang Home.

--- app/src/main/java/com/xinchaongaymoi/hotelbookingapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/xinchaongaymoi/hotelbookingapp/ui/home/HomeFragment.kt
@@ -29,10 +29,6 @@ private var _binding: FragmentHomeBinding? = null
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textSlideshow
-    accountViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
-    }
     return root
   }
 
