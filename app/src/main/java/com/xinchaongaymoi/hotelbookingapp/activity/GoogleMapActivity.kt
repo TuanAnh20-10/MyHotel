306afa0b7fb39a69335c5b2e4856623af914f8dd
quanphantien <22120369@student.hcmus.edu.vn>
2025-01-11 20:47:50 +0700

    fix google map.

--- app/src/main/java/com/xinchaongaymoi/hotelbookingapp/activity/GoogleMapActivity.kt
+++ app/src/main/java/com/xinchaongaymoi/hotelbookingapp/activity/GoogleMapActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.xinchaongaymoi.hotelbookingapp.R
 
@@ -11,6 +12,9 @@ class GoogleMapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_map)
-
+        WindowCompat.setDecorFitsSystemWindows(
+            window,
+            false
+        )
     }
 }
