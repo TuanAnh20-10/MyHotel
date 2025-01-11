b17b207d6e2de1bf36cb40cbe8106d8b4461fe63
quanphantien <22120369@student.hcmus.edu.vn>
2025-01-12 04:51:12 +0700

    customize markers.

--- app/src/main/java/com/xinchaongaymoi/hotelbookingapp/activity/GoogleMapActivity.kt
+++ app/src/main/java/com/xinchaongaymoi/hotelbookingapp/activity/GoogleMapActivity.kt
@@ -1,14 +1,44 @@
 package com.xinchaongaymoi.hotelbookingapp.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.MarkerOptions
 import com.xinchaongaymoi.hotelbookingapp.R
+import com.xinchaongaymoi.hotelbookingapp.adapter.MarkerInfoWindowAdapter
+import com.xinchaongaymoi.hotelbookingapp.helper.BitmapHelper
+import com.xinchaongaymoi.hotelbookingapp.model.Place
+import com.xinchaongaymoi.hotelbookingapp.service.PlacesReader
 
 class GoogleMapActivity : AppCompatActivity() {
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+    private val hotelIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.purple_700)
+        BitmapHelper.vectorToBitmap(this, R.drawable.baseline_hotel_24, color)
+    }
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    .icon(hotelIcon)
+            )
+
+            // Set place as the tag on the marker object so it can be referenced within
+            // MarkerInfoWindowAdapter
+            marker?.tag = place
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_map)
@@ -16,5 +46,14 @@ class GoogleMapActivity : AppCompatActivity() {
             window,
             false
         )
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+            // Set custom info window adapter
+            googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        }
+
     }
 }
